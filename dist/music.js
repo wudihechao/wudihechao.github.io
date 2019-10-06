2261ea7be41426dfa692ece51babeb565d270084
wudihechao <wudihechao@163.com>
2019-10-06 22:05:32 +0800

    Site updated: 2019-10-06 22:05:32

--- dist/music.js
+++ dist/music.js
@@ -4,10 +4,10 @@ const ap = new APlayer({
     autoplay: false,
     audio: [
       {
-        name: "PDD洪荒之力",
-        artist: '徐梦圆',
-        url: 'http://up.mcyt.net/?down/39868.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/84.jpg',
+        name: "体面",
+        artist: '于文文',
+        url: 'http://win.web.nf01.sycdn.kuwo.cn/fb079c60da8c231fc2c791eda0258c38/5d99f291/resource/n1/32/94/1843537127.mp3',
+        cover: 'https://gss0.bdstatic.com/-4o3dSag_xI4khGkpoWK1HF6hhy/baike/c0%3Dbaike116%2C5%2C5%2C116%2C38/sign=45ead8f7ba3533fae1bb9b7cc9ba967a/b812c8fcc3cec3fda031f68cdd88d43f8794273f.jpg',
       },
       {
         name: '9420',
