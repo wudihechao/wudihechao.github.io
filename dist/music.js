8cb9f9b5fd697ab5e49c3f8f1800fe17dea949de
wudihechao <wudihechao@163.com>
2019-10-07 15:56:18 +0800

    Site updated: 2019-10-07 15:56:17

--- dist/music.js
+++ dist/music.js
@@ -6,31 +6,31 @@ const ap = new APlayer({
       {
         name: "体面",
         artist: '于文文',
-        url: 'https://win-web-nf01-sycdn.kuwo.cn/4199dbe68f62f04706f5be67cab5de61/5d9a9b11/resource/n3/94/8/3136134011.mp3',
+        url: 'http://win.web.nf01.sycdn.kuwo.cn/3c4b740f9fe0fcf6c2018c166cb7de42/5d9aecfb/resource/n1/32/94/1843537127.mp3',
         cover: 'https://gss0.bdstatic.com/-4o3dSag_xI4khGkpoWK1HF6hhy/baike/c0%3Dbaike116%2C5%2C5%2C116%2C38/sign=45ead8f7ba3533fae1bb9b7cc9ba967a/b812c8fcc3cec3fda031f68cdd88d43f8794273f.jpg',
       },
       {
         name: '白羊',
         artist: '徐秉龙&沈以诚',
-        url: 'https://win-web-nf01-sycdn.kuwo.cn/647f0cbe9cdadfea9959b50daedd23fc/5d9a9ba5/resource/n2/72/58/2811696247.mp3',
+        url: 'http://win-web-nf01-sycdn.kuwo.cn/b857f0b017e6dc52ae4537cc5c80140e/5d9aee60/resource/n2/72/58/2811696247.mp3',
         cover: 'http://img3.kuwo.cn/star/starheads/120/67/74/3204842448.jpg',
       },
       {
         name: '绿色',
         artist: '陈雪凝',
-        url: 'https://sp-sycdn.kuwo.cn/64eabd9b0a59fc2997717033b0339946/5d9a9cdf/resource/n3/79/89/3763381716.mp3',
+        url: 'http://sp-sycdn.kuwo.cn/6a6022d0ccc92782737c1fe80bd51175/5d9aeed2/resource/n3/79/89/3763381716.mp3',
         cover: 'http://img2.kuwo.cn/star/albumcover/120/92/93/306713154.jpg',
       },
       {
         name: '退',
         artist: '蒋蒋',
-        url: 'https://win-web-nf01-sycdn.kuwo.cn/47470cdd6a2899e1d268b3492ff3daa9/5d9a9d31/resource/n3/20/34/3457848894.mp3',
+        url: 'http://win-web-nf01-sycdn.kuwo.cn/1261d541a211d8219067fc4f9c5f2b4a/5d9aef27/resource/n3/20/34/3457848894.mp3',
         cover: 'http://img2.kuwo.cn/star/albumcover/120/3/46/4189090216.jpg',
       },
       {
         name: '恋爱循环',
         artist: '花泽香菜',
-        url: 'https://win-web-nf01-sycdn.kuwo.cn/177e07c7feb2334f8312f60b4e170cb4/5d9a9c1e/resource/n2/60/21/501763569.mp3',
+        url: 'http://win-web-nf01-sycdn.kuwo.cn/af78a7e41d78250b0e4de9e4e55a8d5b/5d9aef6c/resource/n2/60/21/501763569.mp3',
         cover: 'http://img3.kuwo.cn/star/albumcover/120/83/18/648271655.jpg',
       }
     ]
