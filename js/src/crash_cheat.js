14579d0f3de028d13927e9ab5c1d350c2de56e71
wudihechao <wudihechao@163.com>
2019-10-09 15:23:28 +0800

    Site updated: 2019-10-09 15:23:28

--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -3,12 +3,10 @@ var OriginTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        $('[rel="icon"]').attr('href', "/images/favicon.png");
         document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
         clearTimeout(titleTime);
     }
     else {
-        $('[rel="icon"]').attr('href', "/images/favicon.png");
         document.title = '(ฅ>ω<*ฅ) 嘻~又好了~' + OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
