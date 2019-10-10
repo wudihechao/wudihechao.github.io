87131e99fca3edcac9ebc93bb2e4bf49dbc8505d
wudihechao <wudihechao@163.com>
2019-10-10 10:19:55 +0800

    Site updated: 2019-10-10 10:19:54

--- blog/dist/music.js
+++ blog/dist/music.js
@@ -6,32 +6,32 @@ const ap = new APlayer({
       {
         name: "体面",
         artist: '于文文',
-        url: 'https://hewanyue.com/mp3/timian.mp3',
-        cover: 'https://hewanyue.com/mp3/cover/timian.jpg',
+        url: 'https://hewanyue.com/blog/mp3/timian.mp3',
+        cover: 'https://hewanyue.com/blog/mp3/cover/timian.jpg',
       },
       {
         name: '白羊',
         artist: '徐秉龙&沈以诚',
-        url: 'https://hewanyue.com/mp3/baiyang.mp3',
-        cover: 'https://hewanyue.com/mp3/cover/baiyang.jpg',
+        url: 'https://hewanyue.com/blog/mp3/baiyang.mp3',
+        cover: 'https://hewanyue.com/blog/mp3/cover/baiyang.jpg',
       },
       {
         name: '绿色',
         artist: '陈雪凝',
-        url: 'https://hewanyue.com/mp3/lvse.mp3',
-        cover: 'https://hewanyue.com/mp3/cover/lvse.jpg',
+        url: 'https://hewanyue.com/blog/mp3/lvse.mp3',
+        cover: 'https://hewanyue.com/blog/mp3/cover/lvse.jpg',
       },
       {
         name: '退',
         artist: '蒋蒋',
-        url: 'https://hewanyue.com/mp3/tui.mp3',
-        cover: 'https://hewanyue.com/mp3/cover/tui.jpg',
+        url: 'https://hewanyue.com/blog/mp3/tui.mp3',
+        cover: 'https://hewanyue.com/blog/mp3/cover/tui.jpg',
       },
       {
         name: '恋爱循环',
         artist: '花泽香菜',
-        url: 'https://hewanyue.com/mp3/lianaixunhuan.mp3',
-        cover: 'https://hewanyue.com/mp3/cover/lianaixunhuan.jpg',
+        url: 'https://hewanyue.com/blog/mp3/lianaixunhuan.mp3',
+        cover: 'https://hewanyue.com/blog/mp3/cover/lianaixunhuan.jpg',
       }
     ]
 });
