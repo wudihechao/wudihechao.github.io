2f370f19ddcdbce3a97ccf1ac9c351f706dad5db
wudihechao <wudihechao@163.com>
2019-10-20 17:45:09 +0800

    Site updated: 2019-10-20 17:45:07

--- blog/js/src/dytitle.js
+++ blog/js/src/dytitle.js
@@ -1,14 +1,1 @@
-var OriginTitile = document.title;
-var titleTime;
-document.addEventListener('visibilitychange', function () {
-    if (document.hidden) {
-        document.title = 'w(ﾟДﾟ)w 出BUG啦！！！！';
-        clearTimeout(titleTime);
-    }
-    else {
-        document.title = '♪(^∇^*)噫~又好了。。。 ' + OriginTitile;
-        titleTime = setTimeout(function () {
-            document.title = OriginTitile;
-        }, 2000);
-    }
-});
+var titleTime,OriginTitile=document.title;document.addEventListener("visibilitychange",function(){document.hidden?(document.title="w(ﾟДﾟ)w 出BUG啦！！！！",clearTimeout(titleTime)):(document.title="♪(^∇^*)噫~又好了。。。 "+OriginTitile,titleTime=setTimeout(function(){document.title=OriginTitile},2e3))});
